86eb9c08268491a694a348359e0d402b89b9c9a7
1-cret <omayr242994@bue.edu.eg>
2025-03-11 02:36:10 +0200

    Update Enrollment.java

--- SchoolManagementSystem/src/Enrollment.java
+++ SchoolManagementSystem/src/Enrollment.java
@@ -8,4 +8,48 @@ public class Enrollment{
     public enum Status {
         ACTIVE, CANCELLED;
     }
+
+    //Constructors
+    Enrollment(int enrollmentID){
+        this.enrollmentID = enrollmentID;
+    }
+
+    Enrollment(int studentID, int moduleID, Status enrollmentStatus){
+        this.studentID = studentID;
+        this.moduleID = moduleID;
+        this.enrollmentStatus = enrollmentStatus;
+    }
+
+    //Getters and Setters
+    public void setEnrollmentID(int enrollmentID){
+        this.enrollmentID = enrollmentID;
+    }
+
+    public int getEnrollmentID(){
+        return enrollmentID;
+    }
+    
+    public void setStudentID(int enrollmentID){
+        this.studentID = studentID;
+    }
+
+    public int getStudentID(){
+        return studentID;
+    }
+
+    public void setModuleID(int moduleIDID){
+        this.moduleID = moduleID;
+    }
+
+    public int getModuleID(){
+        return moduleID;
+    }
+
+    public void setEnrollmentStatus(Status enrollmentStatus){
+        this.enrollmentStatus = enrollmentStatus;
+    }
+
+    public int getEnrollmentID(){
+        return enrollmentID;
+    }
 }
