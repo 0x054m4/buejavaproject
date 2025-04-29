b6c243e7ce8ec73fe653bae4609fc6a6dbf34a96
OmarjhfkSamir <omarsamirtaha2007@gmail.com>
2025-04-29 22:58:06 +0300

    delete
    jgj

--- SchoolManagementSystem/src/Admin.java
+++ SchoolManagementSystem/src/Admin.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 public class Admin extends Staff {
 
     public Admin(int staffId) {
@@ -5,5 +8,9 @@ public class Admin extends Staff {
     } 
     public Admin(String name, String email, String role, StaffStatus status, String password) {
         super(name, email, role, status, password);
+    }
+
+    void manageAccount(ArrayList<Admin> Admin) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
